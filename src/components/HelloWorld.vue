d9503c3c5646c637f7189d7c59b51d556c0bc36d
DeoDeng-Git <deoseoer@gmail.com>
2021-02-13 11:30:39 +0800

    feat: add toast animation lifehook

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,51 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast"/>
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
     <button @click="showToast = !showToast">Show Toast</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 import Toast from "./Toast.vue";
 import Todos from "./Todos.vue";
 export default {
-  components:{Toast, Todos},
+  components: { Toast, Todos },
   setup() {
-    const showToast = ref(false)
+    const showToast = ref(false);
     return {
-      showToast
-    }
-  }
-}
+      showToast,
+    };
+  },
+};
 </script>
 
 <style>
 
+/* toast enter */
+.toast-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+.toast-enter-active {
+  transition: all 0.3s ease;
+}
+.toast-leave-from {
+  opacity: 1;
+  transition: translateY(0);
+}
+
+.toast-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-leave-active {
+  transition: all 0.3s ease;
+}
 </style>
