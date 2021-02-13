eed24efb6fd16ab94db7149f9fe706d1fb4fe431
DeoDeng-Git <deoseoer@gmail.com>
2021-02-13 11:20:54 +0800

    feat: init projecct

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <Toast v-if="showToast"/>
+    <button @click="showToast = !showToast">Show Toast</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import Toast from "./Toast.vue";
+import Todos from "./Todos.vue";
+export default {
+  components:{Toast, Todos},
+  setup() {
+    const showToast = ref(false)
+    return {
+      showToast
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
